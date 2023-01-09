0ec55f24efa3626181eff861f6d8ccb527986014
梦中梦 <9451454+nzghfjh@user.noreply.gitee.com>
2023-01-09 16:15:18 +0800

    论坛社区——Redis整合及测试

---
+++ commity/src/test/java/com/fjh/commity/RedisTests.java
@@ -0,0 +1,34 @@
+package com.fjh.commity;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@SpringBootTest(classes = CommityApplication.class)
+@RunWith(SpringRunner.class)
+public class RedisTests {
+    @Autowired
+    private RedisTemplate redisTemplate;
+    @Test
+    public void testStrings(){
+        String redisKey = "test:count";
+        redisTemplate.opsForValue().set(redisKey,10);
+
+        System.out.println(redisTemplate.opsForValue().get(redisKey));
+        System.out.println(redisTemplate.opsForValue().increment(redisKey));
+        System.out.println(redisTemplate.opsForValue().decrement(redisKey));
+    }
+    @Test
+    public void testHash(){
+        String redisKey = "test:user";
+        redisTemplate.opsForHash().put(redisKey,"id",1);
+        redisTemplate.opsForHash().put(redisKey,"username","zhangsan");
+        System.out.println(redisTemplate.opsForHash().get(redisKey,"id"));
+        System.out.println(redisTemplate.opsForHash().get(redisKey,"username"));
+    }
+
+
+}
